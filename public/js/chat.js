da54d753d9f9f612858aea608de2a551df8c8050
TheBigR <nevoroy@gmail.com>
2021-04-24 15:20:36 +0300

    sidebar completed

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages')
 //templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
+const sidebarTemplate = document.querySelector('#sideBar-template').innerHTML
 
 //option
 const { username, room } = Qs.parse(location.search, {
@@ -33,6 +34,14 @@ socket.on('locationMessage', (message) => {
     createdAt: moment(message.createdAt).format('HH:mm:ss'),
   })
   $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('roomData', ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  })
+  document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit', (e) => {
