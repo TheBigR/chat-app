0ae0bd4edb206355ee99fab0b02646b5993d7c88
TheBigR <nevoroy@gmail.com>
2021-04-24 09:55:52 +0300

    timestamps added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const port = process.env.PORT
 const app = express()
@@ -27,37 +28,36 @@ app.use(function (req, res, next) {
 })
 
 app.use(express.json())
-
 app.use(express.static(publicDirectoryPath))
-
-const message = 'Welcome!'
 
 io.on('connection', (socket) => {
   console.log('new websocket connection')
 
-  socket.emit('message', message)
+  socket.emit('message', generateMessage('Welcome!'))
 
-  socket.broadcast.emit('message', 'A new user has joined')
+  socket.broadcast.emit('message', generateMessage('A new user has joined'))
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter()
     if (filter.isProfane(message)) {
       return callback('No bad words allowed!')
     }
-    io.emit('message', message)
+    io.emit('message', generateMessage(message))
     callback()
   })
 
   socket.on('sendLocation', (position, callback) => {
     io.emit(
       'locationMessage',
-      `https://google.com/maps?q=${position.lat},${position.lon} `,
+      generateLocationMessage(
+        `https://google.com/maps?q=${position.lat},${position.lon} `,
+      ),
     )
     callback()
   })
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left.')
+    io.emit('message', generateMessage('A user has left.'))
   })
 })
 server.listen(port, () => {
